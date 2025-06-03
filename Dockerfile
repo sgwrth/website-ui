94336fd614d8895ba2405b6754385c664ff2ce12
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-06-03 19:53:01 +0200

    * add: Dockerfile and .dockerignore

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:lts-alpine3.22
+WORKDIR /app
+COPY package*.json ./
+RUN npm install 
+COPY . .
+RUN npm run build
+EXPOSE 3000
+CMD ["node", ".output/server/index.mjs"]
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
+const store = useUserStore()
 const get = getRequest()
 const posts = ref([])
- onMounted(async () => {
+
+onMounted(async () => {
  	posts.value = await get('post')
- })
+})
 </script>
 
 <template>
 	<h1>les posts</h1>
-	<div v-for="post in posts" :key="post.id">
+	<div v-if="store.name !== ''" v-for="post in posts" :key="post.id">
 		<div>{{ post.title }}</div>
 		<div>{{ post.text }}</div>
 	</div>
+	<div v-else>log in to see poats</div>
 </template>
