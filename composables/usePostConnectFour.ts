b8d987b96d22f3c19aea8f5979567312f033367e
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-09-07 19:35:14 +0200

    * change: Connect Four temporarily connects directly to Python server * change: select column to place token via mouseover in and send move   with click * change: applied some DRY to template rendering the Connect Four board

--- composables/usePostConnectFour.ts
+++ composables/usePostConnectFour.ts
@@ -3,10 +3,12 @@ const store = useUserStore()
 
 export default function usePostConnectFour() {
     return async (payload: string) => {
-        const response = await $fetch(`${runtimeConfig.public.apiUrl}/connectfour`, {
+        // const response = await $fetch(`${runtimeConfig.public.apiUrl}/connectfour`, {
+        const response = await $fetch('http://172.19.39.225:5000/', {
             method: 'POST',
             headers: {
-                Authorization: `Bearer ${store.token}`
+                Authorization: `Bearer ${store.token}`,
+                'Content-Type': 'application/json',
             },
             body: payload
         })
--- pages/connect-four.vue
+++ pages/connect-four.vue
@@ -8,30 +8,32 @@ const board: Ref<Array<Array<string>>> = ref([
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
     [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', 'X', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
 ])
 
 function convertBoardSymbol(symbol: string): string {
     switch (symbol) {
     case ' ':
-        return '_'
+        return ' '
     case 'X':
-        return 'F'
+        return 'X'
     case 'O':
-        return 'B'
+        return 'O'
     default:
         return '?'
     }
 }
 
-function makeMove(col: number) {
-    console.log(`move col ${col}`)
+async function makeMove(col: number) {
+    gameData.value.move = col
+    gameData.value = await postConnectFour(gameData.value)
+    console.log(gameData.value)
+    board.value = gameData.value.board
 }
 
 onMounted(async () => {
     gameData.value = await postConnectFour({'message': 'hi'})
     board.value = gameData.value.board
-    console.log(board.value)
 })
 </script>
 
@@ -42,110 +44,44 @@ onMounted(async () => {
         </div>
 
         <div class="board">
-            <div class="col1" v-on:click="makeMove(1)">
-                <div class="cell">{{ convertBoardSymbol(board[0][0]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[1][0]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[2][0]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[3][0]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[4][0]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[5][0]) }}</div>
-            </div>
-            <div class="col2" v-on:click="makeMove(2)">
-                <div class="cell">{{ convertBoardSymbol(board[0][1]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[1][1]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[2][1]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[3][1]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[4][1]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[5][1]) }}</div>
-            </div>
-            <div class="col3" v-on:click="makeMove(3)">
-                <div class="cell">{{ convertBoardSymbol(board[0][2]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[1][2]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[2][2]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[3][2]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[4][2]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[5][2]) }}</div>
-            </div>
-            <div class="col4" v-on:click="makeMove(4)">
-                <div class="cell">{{ convertBoardSymbol(board[0][3]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[1][3]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[2][3]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[3][3]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[4][3]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[5][3]) }}</div>
-            </div>
-            <div class="col5" v-on:click="makeMove(5)">
-                <div class="cell">{{ convertBoardSymbol(board[0][4]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[1][4]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[2][4]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[3][4]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[4][4]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[5][4]) }}</div>
-            </div>
-            <div class="col6" v-on:click="makeMove(6)">
-                <div class="cell">{{ convertBoardSymbol(board[0][5]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[1][5]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[2][5]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[3][5]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[4][5]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[5][5]) }}</div>
-            </div>
-            <div class="col7" v-on:click="makeMove(7)">
-                <div class="cell">{{ convertBoardSymbol(board[0][6]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[1][6]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[2][6]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[3][6]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[4][6]) }}</div>
-                <div class="cell">{{ convertBoardSymbol(board[5][6]) }}</div>
-            </div>
-        </div>
 
-        <!--
-        <div class="board">
-            <div class="cell col1">{{ convertBoardSymbol(board[0][0]) }}</div>
-            <div class="cell col2">{{ convertBoardSymbol(board[0][1]) }}</div>
-            <div class="cell col3">{{ convertBoardSymbol(board[0][2]) }}</div>
-            <div class="cell col4">{{ convertBoardSymbol(board[0][3]) }}</div>
-            <div class="cell col5">{{ convertBoardSymbol(board[0][4]) }}</div>
-            <div class="cell col6">{{ convertBoardSymbol(board[0][5]) }}</div>
-            <div class="cell col7">{{ convertBoardSymbol(board[0][6]) }}</div>
-            <div class="cell col1">{{ convertBoardSymbol(board[1][0]) }}</div>
-            <div class="cell col2">{{ convertBoardSymbol(board[1][0]) }}</div>
-            <div class="cell col3">{{ convertBoardSymbol(board[1][1]) }}</div>
-            <div class="cell col4">{{ convertBoardSymbol(board[1][2]) }}</div>
-            <div class="cell col5">{{ convertBoardSymbol(board[1][3]) }}</div>
-            <div class="cell col6">{{ convertBoardSymbol(board[1][4]) }}</div>
-            <div class="cell col7">{{ convertBoardSymbol(board[1][5]) }}</div>
-            <div class="cell col1">{{ convertBoardSymbol(board[2][6]) }}</div>
-            <div class="cell col2">{{ convertBoardSymbol(board[2][1]) }}</div>
-            <div class="cell col3">{{ convertBoardSymbol(board[2][2]) }}</div>
-            <div class="cell col4">{{ convertBoardSymbol(board[2][3]) }}</div>
-            <div class="cell col5">{{ convertBoardSymbol(board[2][4]) }}</div>
-            <div class="cell col6">{{ convertBoardSymbol(board[2][5]) }}</div>
-            <div class="cell col7">{{ convertBoardSymbol(board[2][6]) }}</div>
-            <div class="cell col1">{{ convertBoardSymbol(board[3][1]) }}</div>
-            <div class="cell col2">{{ convertBoardSymbol(board[3][2]) }}</div>
-            <div class="cell col3">{{ convertBoardSymbol(board[3][3]) }}</div>
-            <div class="cell col4">{{ convertBoardSymbol(board[3][4]) }}</div>
-            <div class="cell col5">{{ convertBoardSymbol(board[3][5]) }}</div>
-            <div class="cell col6">{{ convertBoardSymbol(board[3][6]) }}</div>
-            <div class="cell col7">{{ convertBoardSymbol(board[3][0]) }}</div>
-            <div class="cell col1">{{ convertBoardSymbol(board[4][0]) }}</div>
-            <div class="cell col2">{{ convertBoardSymbol(board[4][1]) }}</div>
-            <div class="cell col3">{{ convertBoardSymbol(board[4][2]) }}</div>
-            <div class="cell col4">{{ convertBoardSymbol(board[4][3]) }}</div>
-            <div class="cell col5">{{ convertBoardSymbol(board[4][4]) }}</div>
-            <div class="cell col6">{{ convertBoardSymbol(board[4][5]) }}</div>
-            <div class="cell col7">{{ convertBoardSymbol(board[4][6]) }}</div>
-            <div class="cell col1">{{ convertBoardSymbol(board[5][0]) }}</div>
-            <div class="cell col2">{{ convertBoardSymbol(board[5][1]) }}</div>
-            <div class="cell col3">{{ convertBoardSymbol(board[5][2]) }}</div>
-            <div class="cell col4">{{ convertBoardSymbol(board[5][3]) }}</div>
-            <div class="cell col5">{{ convertBoardSymbol(board[5][4]) }}</div>
-            <div class="cell col6">{{ convertBoardSymbol(board[5][5]) }}</div>
-            <div class="cell col7">{{ convertBoardSymbol(board[5][6]) }}</div>
+            <!-- Loop is 1-indexed, hence the '[i - 1]'. -->
+            <div class="col1" v-on:click="makeMove(0)">
+                <div v-for="i in 6" :key="i" class="cell">
+                    {{ convertBoardSymbol(board[i - 1][0]) }}
+                </div>
+            </div>
+            <div class="col2" v-on:click="makeMove(1)">
+                <div v-for="i in 6" :key="i" class="cell">
+                    {{ convertBoardSymbol(board[i - 1][1]) }}
+                </div>
+            </div>
+            <div class="col3" v-on:click="makeMove(2)">
+                <div v-for="i in 6" :key="i" class="cell">
+                    {{ convertBoardSymbol(board[i - 1][2]) }}
+                </div>
+            </div>
+            <div class="col4" v-on:click="makeMove(3)">
+                <div v-for="i in 6" :key="i" class="cell">
+                    {{ convertBoardSymbol(board[i - 1][3]) }}
+                </div>
+            </div>
+            <div class="col5" v-on:click="makeMove(4)">
+                <div v-for="i in 6" :key="i" class="cell">
+                    {{ convertBoardSymbol(board[i - 1][4]) }}
+                </div>
+            </div>
+            <div class="col6" v-on:click="makeMove(5)">
+                <div v-for="i in 6" :key="i" class="cell">
+                    {{ convertBoardSymbol(board[i - 1][5]) }}
+                </div>
+            </div>
+            <div class="col7" v-on:click="makeMove(6)">
+                <div v-for="i in 6" :key="i" class="cell">
+                    {{ convertBoardSymbol(board[i - 1][6]) }}
+                </div>
+            </div>
         </div>
-        -->
 
     </div>
 </template>
@@ -160,6 +96,8 @@ onMounted(async () => {
 .cell {
     border: solid 1px #333;
     text-align: center;
+    align-items: center;
+    height: 1.5rem;
 }
 .col1:hover {
     background-color: #300000;
