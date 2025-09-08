64bb14ff8161a9327ca54719397779a43d4bac93
Andreas Siegwarth <andreassiegwarth@startmail.com>
2025-09-08 20:30:03 +0200

    * change: Connect Four connects to Laravel backend now

--- composables/usePostConnectFour.ts
+++ composables/usePostConnectFour.ts
@@ -3,8 +3,7 @@ const store = useUserStore()
 
 export default function usePostConnectFour() {
     return async (payload: string) => {
-        // const response = await $fetch(`${runtimeConfig.public.apiUrl}/connectfour`, {
-        const response = await $fetch('http://172.19.39.225:5000/', {
+        const response = await $fetch(`${runtimeConfig.public.apiUrl}/connectfour`, {
             method: 'POST',
             headers: {
                 Authorization: `Bearer ${store.token}`,
